e3e208cbc81ec25c5b7c89b28d709d0aa5792dd3
Arthaud <arthaudp33@gmail.com>
2023-01-13 12:54:20 +0100

    feat: undo container reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,16 @@ module.exports = {
       sans: ['Marianne', ...defaultTheme.fontFamily.sans],
       display: ['Montserrat']
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '8rem',
+        '2xl': '12rem',
+      },
+    },
 
   },
   plugins: [
