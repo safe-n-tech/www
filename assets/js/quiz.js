b639986908ec50c7d705610e82e5871ca145d01c
ArGwaster <148948313+ArGwaster@users.noreply.github.com>
2026-06-05 15:10:18 +0200

    fix quizz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,7 @@
 initQuiz({
   storageKey: 'safe-n-tech-questions',
   questionsCount: 10,
+  filter: q => q.quizCultureGenerale !== true,
   sentenceLevels: [
     "Vos appareils et vos données sont un trésor pour les pirates. Attention, ils sont parés à l'abordage !",
     "Vous y êtes presque ! Mais pour les pirates, c'est comme si vous aviez fermé votre maison à clef, et laissé une fenêtre ouverte.",
